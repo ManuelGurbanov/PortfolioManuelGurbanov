5e48cbd091dac268b8e5dfe25f7fe166cadd0319
ManuelGurbanov <manuelgurbanov@gmail.com>
2024-05-26 19:15:35 -0300

    removed bg from loading screen

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -7,18 +7,18 @@ const LoadingScreen = ({ onFinish }) => {
         const timer = setTimeout(() => {
             setFadeOut(true);
             setTimeout(onFinish, 1000); // Duración del fade out
-        }, 2000); // Duración de la pantalla de carga
+        }, 1200); // Duración de la pantalla de carga
 
         return () => clearTimeout(timer);
     }, [onFinish]);
 
     return (
-        <div className={`fixed top-0 left-0 w-full h-full bg-opacity-90 bg-mycolors-bg flex items-center justify-center transition-opacity duration-1000 ${fadeOut ? 'opacity-0' : 'opacity-100'}`}>
+        <div className={`fixed top-0 left-0 w-full h-full bg-opacity-90 flex items-center justify-center transition-opacity duration-1000 ${fadeOut ? 'opacity-0' : 'opacity-100'}`}>
             <div className="m-5 text-2xl font-semibold text-mycolors-2 max-w-[80vw]">
                 <h3>
                     Bienvenidos a mi portfolio
                 </h3>
-                <h1 className='text-white font-bolder text-7xl'>
+                <h1 className='text-6xl text-white font-bolder'>
                     Manuel Gurbanov
                 </h1>
             </div>
